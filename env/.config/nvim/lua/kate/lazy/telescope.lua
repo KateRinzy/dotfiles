162aac4be06bf3300fe0a00fc717606adccb256f
Kate <alicelegare@outlook.com>
2025-09-10 21:41:43 -0400

    added mapping to open PDFs using telescope (using zathura as the pdf viewer)

--- env/.config/nvim/lua/kate/lazy/telescope.lua
+++ env/.config/nvim/lua/kate/lazy/telescope.lua
@@ -1,41 +1,57 @@
 return {
-    "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+	tag = "0.1.5",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
+	dependencies = {
+		"nvim-lua/plenary.nvim"
+	},
 
-    config = function()
-        require('telescope').setup({})
+	config = function()
+		require('telescope').setup({})
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('v', '<leader>pws', function()
-            -- save current register
-            local save_reg = vim.fn.getreg('"')
-            local save_type = vim.fn.getregtype('"')
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+		vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
+		vim.keymap.set('n', '<leader>pws', function()
+			local word = vim.fn.expand("<cword>")
+			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set('v', '<leader>pws', function()
+			-- save current register
+			local save_reg = vim.fn.getreg('"')
+			local save_type = vim.fn.getregtype('"')
 
-            -- yank visual selection into " register
-            vim.cmd('normal! ""y')
+			-- yank visual selection into " register
+			vim.cmd('normal! ""y')
 
-            -- get yanked text
-            local selection = vim.fn.getreg('"')
+			-- get yanked text
+			local selection = vim.fn.getreg('"')
 
-            -- restore register
-            vim.fn.setreg('"', save_reg, save_type)
+			-- restore register
+			vim.fn.setreg('"', save_reg, save_type)
 
-            require('telescope.builtin').grep_string({ search = selection })
-        end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<C-b>', builtin.buffers, {})
-    end
+			require('telescope.builtin').grep_string({ search = selection })
+		end)
+		vim.keymap.set('n', '<leader>ps', function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
+		vim.keymap.set('n', '<C-b>', builtin.buffers, {})
+		vim.keymap.set("n", "<leader>pdf", function()
+			require("telescope.builtin").find_files({
+				prompt_title = "Find PDF",
+				find_command = { "fd", "--type", "f", "--extension", "pdf" },
+				attach_mappings = function(_, map)
+					map("i", "<CR>", function(prompt_bufnr)
+						local action_state = require("telescope.actions.state")
+						local actions = require("telescope.actions")
+						local selection = action_state.get_selected_entry()
+						actions.close(prompt_bufnr)
+						vim.fn.jobstart({ "zathura", selection.path }, { detach = true })
+					end)
+					return true
+				end,
+			})
+		end, {})
+	end
 }
